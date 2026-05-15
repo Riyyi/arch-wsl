467db4035ee6186f30d0a6df36aadb6364a36b94
Riyyi <riyyi3@gmail.com>
2026-05-15 09:28:26 +0200

    Add private zsh aliases

--- modules/features/sops/secrets/arch-vm/zshrc-extended.sh
+++ modules/features/sops/secrets/arch-vm/zshrc-extended.sh
@@ -1,5 +1,5 @@
 {
-	"data": "ENC[AES256_GCM,data:XJlPJE0jJBsw5w==,iv:EHGYNI6/mvvBFwrd6YJPnagPJfP9I50jdK63eWiJ9bI=,tag:Ra4xNQ2hW2rf2mXsaKF4WA==,type:str]",
+	"data": "ENC[AES256_GCM,data:sroCKeNOiAP792hgZe7zgSyeKXT9z0cLvWjpSyU/vadcUfoERjyuZaGHcZNq8Htaw5gge340TeeP5MSJ12Md+Ik9SbmPoIz4y/aLD5g4H/hFR4dMZGwmnLm/Rx6jCw5r7pZD4xIkq1BhOz1lobqEVpspTm4XSKg6IU01NB1RJjB6n6e6xu3QE+UVUhbq/C3+n6u5CAB+PC+4ZTFh4bHiShmQck26vbs1o/LC92ILC545AKMslGS8vBmVjDx3NXrVBWUrF/4drbzerDl6kVTIC4vzr3rqFg5pW76cFuEjvQDeqbqf29GOrgy5ytGjOBoiwc1iIZ0tcMpSYJwJsSzyu7w8JGrYQ/L9Ras2pMOvOYWYus/Wc2Ls5WZrvp3UzFY3ZHmx9+FNtQhsCyUb+Qysf1iUOtLO5KI74IML+hQZ1Gvv8YrXait9eD+i6gYzAbQ0cbERddJ8DuxmgR+D/+KfG5CcIJOy5ooi91JFbegIqNEK,iv:N5juLhxNooj3ReLmjAk2wt/EVDeR+cISDK2XyHDZmQg=,tag:b80aSPhRuC9hXlad/yjzLA==,type:str]",
 	"sops": {
 		"age": [
 			{
@@ -7,8 +7,8 @@
 				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBmV0JqN2VHZWNxTHN4WUto\nYVdXSVRxN0l3cCtkVEc0SWJ6b1Qra0MzUHdVCkYxd0puT1U0Tk9aYmNWRjUvYlZo\nSkJSMWFsOGM2NWxic1ZrZGtoL2haWlEKLS0tIEtjcVljS0FoSEFDV0ovSnhzRUMw\ncWZmQnNKM25qMXhXUjBEMzM0NVpPUVEKVEq0ghC+Brhq2ZdcD+9+27z6oMRVj66V\nFk/AqBHu2jxdSk+mO3I3uoueTAV0WlyTKcDh+QnF4Dei55NmrwGoHQ==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2026-05-11T10:07:15Z",
-		"mac": "ENC[AES256_GCM,data:GVQy3I3QW5iSw+z7ttanwOeKPIxpAyCEw0ikBn/P4bGq7HdiORQIyJtQQqfgAnGjf8mFErykrHArPZmkSLNmTpbHATNh8Ax2EhhPkPMGy2vX6lK9pnAHVbt0E4UiRE2UyR/c+CJoGviXb5mzJ0yBhCsMCBvv+8YthS1Yrpy9wXI=,iv:ohBjFkk5DzlnEtxxOreTwqWVcxEiNmNZzJ0halzkyAw=,tag:0OCZFfwUmhDwbh1aR2lU5Q==,type:str]",
+		"lastmodified": "2026-05-15T07:27:33Z",
+		"mac": "ENC[AES256_GCM,data:zm9/bb6kcstIqdNY8lsJxixjn0Was918X1BDbHzyrx3oKGMAtThMogzHWgGJsMbq4eNooufAArKkrk53R6XPSn86WJOHUKxrTGwrRUd4HD7YAg5vHq7gwxEr23tXtIvK3ExgdSd4/korsxft/Ia1vqRAyqBuFaKQ9Uu4sERzad8=,iv:+DVjg4r2vcy+cfj3reiQXqB0pPzNtjN7q2xajOZeoQI=,tag:bZsSKi2ECZtl96+yMVatVA==,type:str]",
 		"unencrypted_suffix": "_unencrypted",
 		"version": "3.12.1"
 	}
