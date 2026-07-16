21d3468ac50fa01a1a0e16050f5e92a2d6b4162d
Riyyi <riyyi3@gmail.com>
2026-07-16 16:51:05 +0200

    Add docs about package installs

--- modules/hosts/ubuntu-laptop/bootstrap.sh
+++ modules/hosts/ubuntu-laptop/bootstrap.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-#..
+# The following is installed externally:
+
+# PPA's added
+# /etc/apt/sources.list.d
+# - claude-destkop.list
+# - microsoft-prod.list
+# - vscode.sources
+
+# Installed from PPA
+# - claude-desktop
+# - code (VS Code)
+# - mdatp (Microsoft Defender)
+
+# Others, these will never get updated
+# - dotnet
+#   https://dot.net/v1/dotnet-install.sh --channel 10.0
+# - node (nvm)
+#   https://nodejs.org/en/download
