e0fb373c1db51f8fec9c02f79404a732c9bd7be7
Riyyi <riyyi3@gmail.com>
2026-07-20 14:16:12 +0200

    Rewrite dconf settings, create previous-workspace gnome extension

--- modules/features/gnome/gnome.nix
+++ modules/features/gnome/gnome.nix
@@ -10,6 +10,35 @@
       modDir = dirOf __curPos.file;
       entries = builtins.readDir "${modDir}/dotfiles/Pictures/Wallpapers";
       files = builtins.filter (name: entries.${name} == "regular") (builtins.attrNames entries);
+
+      # GNOME 46 extension that tracks current/previous workspace and exposes
+      # a "switch to previous workspace" keybinding.
+      workspacePreviousExt = pkgs.stdenv.mkDerivation {
+        pname = "gnome-shell-extension-workspace-previous";
+        version = "1";
+        src = ./dotfiles/.local/share/gnome-shell/extensions/workspace-previous-src;
+        # Source is already a flat directory; no unpacking step needed.
+        dontUnpack = true;
+
+        nativeBuildInputs = [ pkgs.glib ];
+
+        installPhase = ''
+          runHook preInstall
+
+          uuid="workspace-previous@dotfiles"
+          dst="$out/share/gnome-shell/extensions/$uuid"
+          mkdir -p "$dst"
+          cp -r "$src"/. "$dst/"
+          # Store source files are read-only; make the install tree writable
+          # so glib-compile-schemas can drop its compiled output.
+          chmod -R u+w "$dst"
+
+          # gschema needs to be compiled for the shell to pick it up.
+          glib-compile-schemas "$dst/schemas"
+
+          runHook postInstall
+        '';
+      };
     in
     {
 
@@ -29,132 +58,204 @@
         ++ lib.optionals (config.preferences.distro == "ubuntu") [
         ];
 
-      home.activation.gnome = lib.hm.dag.entryAfter [ "aptPackages" ] ''
+      dconf.settings = lib.mkMerge [
         # Workspaces
-        /bin/gsettings set org.gnome.mutter dynamic-workspaces false
-        /bin/gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
+        {
+          "org/gnome/mutter" = {
+            dynamic-workspaces = false;
+          };
+          "org/gnome/desktop/wm/preferences" = {
+            num-workspaces = 10;
+          };
+        }
 
         # Dock
-        /bin/gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-        /bin/gsettings set org.gnome.shell.extensions.dash-to-dock dock-position "'BOTTOM'"
-        /bin/gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+        {
+          "org/gnome/shell/extensions/dash-to-dock" = {
+            dock-fixed = false;
+            dock-position = "BOTTOM";
+            extend-height = false;
+          };
+        }
 
         # Desktop
-        /bin/gsettings set org.gnome.shell.extensions.ding show-home false
-        /bin/gsettings set org.gnome.desktop.background show-deskop-icons false
+        {
+          "org/gnome/shell/extensions/ding" = {
+            show-home = false;
+          };
+          "org/gnome/desktop/background" = {
+            show-desktop-icons = false;
+          };
+        }
 
         # Style
-        /bin/gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+        {
+          "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+          };
+        }
 
         # Wallpaper
-        /bin/gsettings set org.gnome.desktop.background picture-uri \
-          "file://${config.home.homeDirectory}/Pictures/Wallpapers/wallpaper.png"
-        /bin/gsettings set org.gnome.desktop.background picture-uri-dark \
-          "file://${config.home.homeDirectory}/Pictures/Wallpapers/wallpaper.png"
-        /bin/gsettings set org.gnome.desktop.background picture-options "zoom"
+        {
+          "org/gnome/desktop/background" = {
+            picture-uri = "file://${config.home.homeDirectory}/Pictures/Wallpapers/wallpaper.png";
+            picture-uri-dark = "file://${config.home.homeDirectory}/Pictures/Wallpapers/wallpaper.png";
+            picture-options = "zoom";
+          };
+        }
 
         # ----------------------------------
 
         # Input tweaks
-        /bin/gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
-        #/bin/gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']" #TODO: uncomment
+        {
+          "org/gnome/desktop/wm/preferences" = {
+            resize-with-right-button = true;
+          };
+          # TODO: uncomment
+          # "org/gnome/desktop/input-sources" = {
+          #   xkb-options = [ "caps:swapescape" ];
+          # };
+        }
+
+        # ----------------------------------
+
+        # Enable the workspace-previous GNOME extension.
+        {
+          "org/gnome/shell" = {
+            enabled-extensions = [
+              "workspace-previous@dotfiles"
+            ];
+            disable-user-extensions = false;
+          };
+          # Focus previous workspace
+          "org/gnome/shell/extensions/workspace-previous" = {
+            switch-to-previous-workspace = [ "<Super>grave" ];
+          };
+        }
 
         # ----------------------------------
 
         # Remap super -> super+d for Activities Overview page
-        /bin/gsettings set org.gnome.mutter overlay-key ""
-        /bin/gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>d']"
+        {
+          "org/gnome/mutter" = {
+            overlay-key = "";
+          };
+          "org/gnome/shell/keybindings" = {
+            toggle-overview = [ "<Super>d" ];
+          };
+        }
 
         # Disable conflicting default bindings
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-5 "[]"
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-6 "[]"
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-7 "[]"
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"
-        /bin/gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
+        {
+          "org/gnome/shell/keybindings" = {
+            switch-to-application-1 = [ ];
+            switch-to-application-2 = [ ];
+            switch-to-application-3 = [ ];
+            switch-to-application-4 = [ ];
+            switch-to-application-5 = [ ];
+            switch-to-application-6 = [ ];
+            switch-to-application-7 = [ ];
+            switch-to-application-8 = [ ];
+            switch-to-application-9 = [ ];
+          };
+        }
 
         # Disable dock bindings, these conflict with binding super+<num>
-        /bin/gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+        {
+          "org/gnome/shell/extensions/dash-to-dock" = {
+            hot-keys = false;
+          };
+        }
 
         # ----------------------------------
 
         # --- Window --- #
 
-        /bin/gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
+        {
+          "org/gnome/desktop/wm/keybindings" = {
+            close = [ "<Super>q" ];
 
-        #-- State/flags --#
+            #-- State/flags --#
 
-        # Toggle fullscreen mode
-        /bin/gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>g']"
+            # Toggle fullscreen mode
+            toggle-fullscreen = [ "<Super>f" ];
+            toggle-maximized = [ "<Super>g" ];
 
-        #-- Focus --#
+            #-- Focus --#
 
-        # Focus workspace
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
+            # Focus workspace
+            switch-to-workspace-1 = [ "<Super>1" ];
+            switch-to-workspace-2 = [ "<Super>2" ];
+            switch-to-workspace-3 = [ "<Super>3" ];
+            switch-to-workspace-4 = [ "<Super>4" ];
+            switch-to-workspace-5 = [ "<Super>5" ];
+            switch-to-workspace-6 = [ "<Super>6" ];
+            switch-to-workspace-7 = [ "<Super>7" ];
+            switch-to-workspace-8 = [ "<Super>8" ];
+            switch-to-workspace-9 = [ "<Super>9" ];
+            switch-to-workspace-10 = [ "<Super>0" ];
 
-        # Focus previous/next workspace
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>minus']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>equal']"
+            # Focus previous/next workspace
+            switch-to-workspace-left = [ "<Super>minus" ];
+            switch-to-workspace-right = [ "<Super>equal" ];
 
-        #-- Move --#
+            #-- Move --#
 
-        # Move window to workspace
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Shift><Super>1']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Shift><Super>2']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Shift><Super>3']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Shift><Super>4']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 "['<Shift><Super>5']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 "['<Shift><Super>6']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 "['<Shift><Super>7']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<Shift><Super>8']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Shift><Super>9']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Shift><Super>0']"
+            # Move window to workspace
+            move-to-workspace-1 = [ "<Shift><Super>1" ];
+            move-to-workspace-2 = [ "<Shift><Super>2" ];
+            move-to-workspace-3 = [ "<Shift><Super>3" ];
+            move-to-workspace-4 = [ "<Shift><Super>4" ];
+            move-to-workspace-5 = [ "<Shift><Super>5" ];
+            move-to-workspace-6 = [ "<Shift><Super>6" ];
+            move-to-workspace-7 = [ "<Shift><Super>7" ];
+            move-to-workspace-8 = [ "<Shift><Super>8" ];
+            move-to-workspace-9 = [ "<Shift><Super>9" ];
+            move-to-workspace-10 = [ "<Shift><Super>0" ];
 
-        # Move previous/next workspace
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Shift><Super>minus']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Shift><Super>equal']"
+            # Move previous/next workspace
+            move-to-workspace-left = [ "<Shift><Super>minus" ];
+            move-to-workspace-right = [ "<Shift><Super>equal" ];
 
-        # Move to previous/next monitor
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "['<Shift><Super>bracketleft']"
-        /bin/gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right "['<Shift><Super>bracketright']"
-      '';
-
-      dconf.settings = {
-        "org/gnome/settings-daemon/plugins/media-keys" = {
-          custom-keybindings = [
-            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-          ];
-        };
-
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-          name = "Terminal";
-          command = "ghostty";
-          binding = "<Super>Return";
-        };
-      };
-
-      home.file = builtins.listToAttrs (
-        # Deploy wallpapers from repo to home directory
-        map (file: {
-          name = "Pictures/Wallpapers/${file}";
-          value = {
-            source = ./dotfiles/Pictures/Wallpapers + "/${file}";
+            # Move to previous/next monitor
+            move-to-monitor-left = [ "<Shift><Super>bracketleft" ];
+            move-to-monitor-right = [ "<Shift><Super>bracketright" ];
           };
-        }) files
-      );
+        }
+
+        # ----------------------------------
+
+        {
+          "org/gnome/settings-daemon/plugins/media-keys" = {
+            custom-keybindings = [
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+            ];
+          };
+          "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+            name = "Terminal";
+            command = "ghostty";
+            binding = "<Super>Return";
+          };
+        }
+      ];
+
+      home.file =
+        builtins.listToAttrs (
+          # Deploy wallpapers from repo to home directory
+          map (file: {
+            name = "Pictures/Wallpapers/${file}";
+            value = {
+              source = ./dotfiles/Pictures/Wallpapers + "/${file}";
+            };
+          }) files
+        )
+        // {
+          # Install the workspace-previous GNOME extension.
+          ".local/share/gnome-shell/extensions/workspace-previous@dotfiles" = {
+            source = "${workspacePreviousExt}/share/gnome-shell/extensions/workspace-previous@dotfiles";
+            recursive = true;
+          };
+        };
 
     };
 }
