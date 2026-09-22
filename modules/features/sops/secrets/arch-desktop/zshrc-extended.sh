14f11748299e599d68eee44be0c9060edf6e2ec8
Riyyi <riyyi3@gmail.com>
2026-09-22 09:06:45 +0200

    Add arch-desktop aliases

--- modules/features/sops/secrets/arch-desktop/zshrc-extended.sh
+++ modules/features/sops/secrets/arch-desktop/zshrc-extended.sh
@@ -1,15 +1,15 @@
 {
-	"data": "ENC[AES256_GCM,data:XyoGfIcrPVEjKrw=,iv:qXm41nSg3HseJyHVvRuxXDECUrrHeCkiAxDiDchOATQ=,tag:VkU/elQQGvDW6qprWZVloQ==,type:str]",
+	"data": "ENC[AES256_GCM,data:WNc4JPbRAVvQ/tDm4KbDaeFPQpB0AWoIbDFgaqVww/IlsVOw1PBEfVP/Lfa2dvPT5YNPhx/tpiRILeNhpkP9Fe7/vax43zCwkCaXqnO0M5vKQ0vg0+usKld3SwN9rnJM0nQSJOJ7g/Fqrd3mzqEbcvkiLkT9J2moLWGZfFbWpbF4al4SiVxGE97l1ZdGJJ24,iv:tg1VUu1W/BemVCLDU4RN7A/JlXP+C+WL5AdbdDGsLn4=,tag:v1nreEb2BvkBK/Ie3oRUBA==,type:str]",
 	"sops": {
 		"age": [
 			{
-				"recipient": "age1fpn5rg8679kc3m9fd5v6fg2tpnnudqumh4alj5mr7qnkra3g5cesdfqz9u",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBmV0JqN2VHZWNxTHN4WUto\nYVdXSVRxN0l3cCtkVEc0SWJ6b1Qra0MzUHdVCkYxd0puT1U0Tk9aYmNWRjUvYlZo\nSkJSMWFsOGM2NWxic1ZrZGtoL2haWlEKLS0tIEtjcVljS0FoSEFDV0ovSnhzRUMw\ncWZmQnNKM25qMXhXUjBEMzM0NVpPUVEKVEq0ghC+Brhq2ZdcD+9+27z6oMRVj66V\nFk/AqBHu2jxdSk+mO3I3uoueTAV0WlyTKcDh+QnF4Dei55NmrwGoHQ==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBmV0JqN2VHZWNxTHN4WUto\nYVdXSVRxN0l3cCtkVEc0SWJ6b1Qra0MzUHdVCkYxd0puT1U0Tk9aYmNWRjUvYlZo\nSkJSMWFsOGM2NWxic1ZrZGtoL2haWlEKLS0tIEtjcVljS0FoSEFDV0ovSnhzRUMw\ncWZmQnNKM25qMXhXUjBEMzM0NVpPUVEKVEq0ghC+Brhq2ZdcD+9+27z6oMRVj66V\nFk/AqBHu2jxdSk+mO3I3uoueTAV0WlyTKcDh+QnF4Dei55NmrwGoHQ==\n-----END AGE ENCRYPTED FILE-----\n",
+				"recipient": "age1fpn5rg8679kc3m9fd5v6fg2tpnnudqumh4alj5mr7qnkra3g5cesdfqz9u"
 			}
 		],
-		"lastmodified": "2026-07-26T08:52:53Z",
-		"mac": "ENC[AES256_GCM,data:M8/Tj6rs7acdn1Sqi+AavMZHjSDVG7jCHtnYiK1JcjeIXapL7zmfXBFCxyNLzGh5DtE9kClezy2iMI019f/kEMwGksnN7jw/xR+0Z28Bxzzy3l3ZrtMgh6lAsKM2E89Uw7J/UFEY+NFICPf/6hswE49K9W9ClPbNk6g+UEG+BQ4=,iv:gQtMivKd1jjrXJsWUkiDPGsfV2cannWnDfimGvCRsxM=,tag:odECtB0Lv3OP9wZ1tD7Itg==,type:str]",
+		"lastmodified": "2026-09-22T06:54:55Z",
+		"mac": "ENC[AES256_GCM,data:0wGcfd9jRn8IENQX1woFBTi7brvierhc8t+874VIpFpEx5OuTfhnLS7ezClt38rAyXlaqaZn1Dw4C67EUSJyb60Yi41El9Yja2mPwHkTOGhnO9YTkKJmDRAl2O201DVF1knSM7L3A3iBTlq/dmNjF1cfSEcntclztzqVxwFuoWA=,iv:H0igsD4lxsJMaevnhgptL3pJuGN7v6+gNrIk3ZkqfK4=,tag:TON3i0KsWOkJOoX+ClZIBg==,type:str]",
 		"unencrypted_suffix": "_unencrypted",
-		"version": "3.12.1"
+		"version": "3.13.2"
 	}
 }
